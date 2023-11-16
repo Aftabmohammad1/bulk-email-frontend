5b663a66462cda7e18dfedc82a38995d8d6d1c68
Aftabmohammad1 <aftab19121@gmail.com>
2023-11-16 18:53:11 +0530

    updated

---
+++ src/Components/Settings/ModalComp.js
@@ -0,0 +1,46 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+function Modalcomp(props) {
+  
+  return (
+    <>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{width:"100%"}}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          How to generate App password for your Email are shown:
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Steps: </h4>
+        <ol>
+          <li>Go to <a href="https://myaccount.google.com">https://myaccount.google.com</a></li>
+          <li>Click <b>Security</b> on your left side tab.</li>
+          <li>Click <b>2-Step Verification</b> on below "Sign in to Google topic"</li>
+          <li>After that Generate Otp,Enter Otp into the input box, hit Enter and complete the two step verification. </li>
+          <li>Come back to the <a href="https://myaccount.google.com/security">https://myaccount.google.com/security</a> page.</li>
+          <li>Click App passwords below the 2-Step verification now.</li>
+          <li>Now it ask you what type of app or device you want to generate the app password in that type some thing what you want related to our App. <b>Eg:Bulk Email</b></li>
+          <li>Then hit generate button.</li>
+          <li>Now it show 12 digit generate app password copy and past it in our website settings password</li>
+          <li>Finally, Compose mail and send from your email id what you set in settings.</li>
+        </ol>
+        <h4>Note:</h4>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={()=>props.onHide()}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+    </>
+  );
+}
+
+
+export default Modalcomp
