9df0b676d96878ca8e34db24c42e0c62902c36b4
Aftabmohammad1 <aftab19121@gmail.com>
2023-12-06 20:53:18 +0530

    updated

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
 //const API = "https://bulk-email-frontend-aftabmohammad1.vercel.app/users"
- const API = "https://bulk-email-backend-wkvm.onrender.com"
+ const API = "https://bulk-email-backend-wkvm.onrender.com/users"
  //const API = "http://localhost:4000/users"
 
  export default API
